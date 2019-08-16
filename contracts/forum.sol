2c5abf61d710d742f1cc134c9817bc1bb9efc7e7
0x10F <me@hadyn.dev>
2019-08-16 04:38:14 -0600

    Reorganized code to be more readable, started on packing

--- contracts/forum.sol
+++ contracts/forum.sol
@@ -1,25 +1,101 @@
 pragma solidity ^0.5.1;
 
-contract Forum {
+contract DChan {
+    // Queue is a generic structure used for singly or doubly linked lists.
+    // NOTE(271): Unwrapping this can decrease the gas cost in nested structures.
+    struct Queue {
+        // The ID of the object that is the head of the queue.
+        uint32 head;
+
+        // The ID of the object that is the tail of the queue.
+        uint32 tail;
+    }
+
+    // Page is a reusable piece of application memory. When a post is submitted, its
+    // content is written to a page of memory and the range of memory it occupies is
+    // recorded.
+    struct Page {
+        // The next page that this page is linked to.
+        uint32 next;
+
+        // The words of memory that comprise this page.
+        bytes32[] words;
+    }
+
+    struct Post {
+        // The next post that this post is linked to.
+        uint32 next;
+
+        // The identifier of the page where the content starts.
+        uint32 pageID;
+
+        // Offset is the starting word in the page that the post occupies.
+        uint32 offset;
+
+        // The length of the post in words.
+        uint32 length;
+    }
+
+    struct Thread {
+        uint32 pagesHead;
+        uint32 pagesTail;
+        uint32 postsHead;
+        uint32 postsTail;
+        uint32 next;
+        uint32 count;
+        uint32 offset;
+    }
+
     uint32 private constant NULL_REF = 0xffffffff;
     bytes32 private constant NULL_WORD = 0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff;
 
-    // MaximumContentLength defines the maximum number of words that the content of a post can be.
+    // The maximum number of words that the content of a post can be.
     uint256 public maximumContentLength;
 
-    // MaximumPages defines the current maximum number of pages that can exist.
+    // The current maximum number of pages that can exist in the contract.
     uint256 public maximumPages;
 
-    // MaximumPosts defines the current maximum number of posts that can exist.
+    // The current maximum number of posts that can exist.
     uint256 public maximumPosts;
 
-    // Maximum threads defines the current maximum number of threads that can exist.
+    // The current maximum number of threads that can exist.
     uint256 public maximumThreads;
 
-    // Initialized is a flag used to indicate of the contract was initialized after being deployed.
+    // Indicates if the contract has been initialized and is live.
     bool private initialized;
 
-    // Init initializes the contract and marks that it is ready to be used.
+    // All of the pages of memory that are contained within the contract mapped by their id.
+    mapping(uint256 => Page) pages;
+
+    // The current number of memory pages in the contract.
+    uint256 pageCount;
+
+    // A queue of all the pages that are currently not being used by threads.
+    Queue unallocatedPages;
+
+    // All of the posts that are contained within the contract mapped by their id.
+    mapping(uint256 => Post) posts;
+
+    // The current number of posts in the contract.
+    uint256 postCount;
+
+    // All the posts that are currently not being used by threads.
+    Queue unallocatedPosts;
+
+    // All of the threads that are contained within the contract mapped by their id.
+    mapping(uint256 => Thread) threads;
+
+    // The current number of threads in the contract.
+    uint256 threadCount;
+
+    // All the threads that are currently being used with the least recent updated thread
+    // at the top of the queue.
+    Queue allocatedThreads;
+
+    // All the threads that are currently not being used.
+    Queue unallocatedThreads;
+
+    // Initializes the contract and marks that it is ready to be used.
     function init(
         uint256 _maximumContentLength,
         uint256 _maximumPages,
@@ -36,6 +112,9 @@ contract Forum {
 
         unallocatedPosts.head = NULL_REF;
         unallocatedPosts.tail = NULL_REF;
+
+        allocatedThreads.head = NULL_REF;
+        allocatedThreads.tail = NULL_REF;
 
         unallocatedThreads.head = NULL_REF;
         unallocatedThreads.tail = NULL_REF;
@@ -54,33 +133,8 @@ contract Forum {
     // content of a post can be.
     function getMaximumContentLengthBytes() public view returns (uint256) {
         return maximumContentLength << 5;
+        // 256 bits/32 bytes per word
     }
-
-    struct Page {
-        uint32 next;
-        bytes32[] words;
-    }
-
-    // Queue is a generic structure used for singly or doubly linked lists. The
-    // list assumes that the objects referred to in the queue use an 128 bit
-    // identifier. This is to conserve gas when updating the queue references.
-    //
-    // TODO(271): Unrolling can decrease the gas cost in nested structs so this might be unneeded.
-    struct Queue {
-        uint32 head;
-        uint32 tail;
-    }
-
-    // Pages are all of the pages of memory that are contained within the contract.
-    mapping(uint256 => Page) pages;
-
-    // InitializedPageCount is a counter which is equal to the current number of memory
-    // pages that have been initialized.
-    uint256 initializedPageCount;
-
-    // UnallocatedPages is queue of all the pages that are currently not being used
-    // by threads.
-    Queue unallocatedPages;
 
     // InitializeMemory initializes pages of memory and returns the number of
     // pages that were successfully initialized. The number of pages that can
@@ -88,11 +142,11 @@ contract Forum {
     // and the current maximum number of pages allowed to be initialized. If
     // no pages were successfully initialized, this function reverts.
     function initializeMemory(uint256 n) isInitialized public returns (uint256 count) {
-        require(initializedPageCount < maximumPages, "Pages already allocated.");
+        require(pageCount < maximumPages, "pages already allocated");
 
         // Limit the number of pages to being at most the number of pages needed
         // to reach the maximum number of pages.
-        uint256 remaining = maximumPages - initializedPageCount;
+        uint256 remaining = maximumPages - pageCount;
         if (n > remaining) {
             n = remaining;
         }
@@ -100,7 +154,7 @@ contract Forum {
         // Copy the current number of initialized pages into local
         // memory. This is so that we can update it as we execute
         // the function and then write the value at the end.
-        uint256 counter = initializedPageCount;
+        uint256 counter = pageCount;
 
         for (uint256 i = 0; i < n; i++) {
             // Page identifiers start at zero so offset from the counter
@@ -137,75 +191,35 @@ contract Forum {
             counter++;
         }
 
-        initializedPageCount = counter;
+        pageCount = counter;
 
         return n;
     }
 
-    // AllocateMemory allocates or reserves a single page of memory and returns
-    // its identifier. If a page could not be allocated then this function
-    // returns NULL_PAGE.
-    function allocateMemory() isInitialized private returns (uint256 id) {
-        uint256 id = unallocatedPages.head;
-
-        if (id == NULL_REF) {
-            return NULL_REF;
-        }
-
-        if (id == unallocatedPages.tail) {
-            unallocatedPages.head = NULL_REF;
-            unallocatedPages.tail = NULL_REF;
-        } else {
-            unallocatedPages.head = pages[id].next;
-        }
-
-        return id;
-    }
-
-    struct Post {
-        // Next is the
-        uint32 next;
-
-        // PageID is the identifier of the page where the post contents start.
-        uint32 pageID;
-
-        // Offset is the starting word in the page that the post occupies.
-        uint32 offset;
-
-        // Length is the length of the post in words.
-        uint32 length;
-    }
-
-    // Posts are all of the posts that are contained within the contract.
-    mapping(uint256 => Post) posts;
-
-    // InitializedPageCount is a counter which is equal to the current number of posts
-    // pages that have been initialized.
-    uint256 initializedPostCount;
-
-    // UnallocatedPosts is queue of all the posts that are currently not being used
-    // by threads.
-    Queue unallocatedPosts;
-
+    // InitializePosts initializes post objects and returns the number of
+    // posts that were successfully initialized. The number of posts that can
+    // be initialized is dependant on the current number of initialized posts
+    // and the current maximum number of posts allowed to be initialized. If
+    // no posts can be initialized, this function reverts.
     function initializePosts(uint256 n) isInitialized public returns (uint256 count) {
-        require(initializedPostCount < maximumPosts, "Posts already allocated.");
+        require(postCount < maximumPosts, "posts already allocated");
 
-        uint256 remaining = maximumPosts - initializedPostCount;
+        uint256 remaining = maximumPosts - postCount;
         if (n > remaining) {
             n = remaining;
         }
 
-        uint256 counter = initializedPostCount;
+        uint256 counter = postCount;
 
         for (uint256 i = 0; i < n; i++) {
             uint256 id = counter + 1;
 
             Post memory post = Post({
-                next:   NULL_REF,
-                pageID: NULL_REF,
-                offset: NULL_REF, // TODO
-                length: NULL_REF
-            });
+                next : NULL_REF,
+                pageID : NULL_REF,
+                offset : NULL_REF,
+                length : NULL_REF
+                });
 
             posts[id] = post;
 
@@ -222,74 +236,42 @@ contract Forum {
             counter++;
         }
 
-        initializedPostCount = counter;
+        postCount = counter;
 
         return n;
     }
 
-    function allocatePost() isInitialized private returns (uint256 id) {
-        uint256 id = unallocatedPosts.head;
-
-        if (id == NULL_REF) {
-            return NULL_REF;
-        }
-
-        if (id == unallocatedPosts.tail) {
-            unallocatedPosts.head = NULL_REF;
-            unallocatedPosts.tail = NULL_REF;
-        } else {
-            unallocatedPosts.head = posts[id].next;
-        }
-
-        return id;
-    }
-
-    struct Thread {
-        uint32 pagesHead;
-        uint32 pagesTail;
-        uint32 postsHead;
-        uint32 postsTail;
-        uint32 next;
-        uint32 count;
-        uint32 offset;
-    }
-
-    // Posts are all of the posts that are contained within the contract.
-    mapping(uint256 => Thread) threads;
-
-    // InitializedPageCount is a counter which is equal to the current number of threads
-    // that have been initialized.
-    uint256 initializedThreadCount;
-
-    // AllocatedThreads is a queue of all the threads that are currently being used with
-    // the least recent updated thread being at the top of the queue.
-    Queue allocatedThreads;
-
-    // UnallocatedThreads is queue of all the threads that are currently not being used.
-    Queue unallocatedThreads;
-
+    // InitializeThreads initializes thread objects and returns the number of
+    // posts that were successfully initialized. The number of threads that can
+    // be initialized is dependant on the current number of initialized threads
+    // and the current maximum number of threads allowed to be initialized. If
+    // no threads can be initialized, this function reverts.
     function initializeThreads(uint256 n) isInitialized public returns (uint256 count) {
-        require(initializedThreadCount < maximumThreads, "Threads already allocated.");
+        require(n > 0, "number of threads must be greater than zero");
+        require(threadCount < maximumThreads, "threads already allocated");
 
-        uint256 remaining = maximumThreads - initializedThreadCount;
+        uint256 remaining = maximumThreads - threadCount;
         if (n > remaining) {
             n = remaining;
         }
 
-        uint256 counter = initializedThreadCount;
+        uint256 counter = threadCount;
 
         for (uint256 i = 0; i < n; i++) {
             uint256 id = counter + 1;
 
+            // Offset can be set to zero since the structure takes up a single word and is non-zero.
+            // If the gas starts acting up because the length of the fields were changed, the
+            // offset being zero is probably why.
             Thread memory thread = Thread({
-                pagesHead: NULL_REF,
-                pagesTail: NULL_REF,
-                postsHead: NULL_REF,
-                postsTail: NULL_REF,
-                next:      NULL_REF,
-                count:     NULL_REF,
-                offset:    0
-            });
+                pagesHead : NULL_REF,
+                pagesTail : NULL_REF,
+                postsHead : NULL_REF,
+                postsTail : NULL_REF,
+                next : NULL_REF,
+                count : NULL_REF,
+                offset : 0
+                });
 
             threads[id] = thread;
 
@@ -306,53 +288,35 @@ contract Forum {
             counter++;
         }
 
-        initializedThreadCount = counter;
+        threadCount = counter;
 
         return n;
     }
 
-    function allocateThread() isInitialized private returns (uint256 id) {
-        uint256 id = unallocatedThreads.head;
-
-        if (id == NULL_REF) {
-            return NULL_REF;
-        }
-
-        if (id == unallocatedThreads.tail) {
-            unallocatedThreads.head = NULL_REF;
-            unallocatedThreads.tail = NULL_REF;
-        } else {
-            unallocatedThreads.head = threads[id].next;
-        }
-
-        return id;
-    }
-
-    function publish(uint256 threadID, bytes32[] memory content) public {
-        require(content.length > 0, "Content is too short");
-        require(content.length <= maximumContentLength, "Content is too long.");
+    function publish(uint256 threadID, bytes32[] memory content) public returns (uint256 threadID) {
+        require(content.length > 0, "content is too short");
+        require(content.length <= maximumContentLength, "content is too long");
 
         // Check if we're creating a new thread, if we are then attempt to allocate a
         // thread. Otherwise, check that the thread exists in the contract.
         bool createThread = threadID == 0x0;
         if (createThread) {
             threadID = allocateThread();
-            require(threadID != NULL_REF, "Failed to allocate thread.");
+            require(threadID != NULL_REF, "failed to allocate thread");
         } else {
-            require(threadID < initializedThreadCount, "Referenced thread does not exist.");
+            require(threadID < threadCount, "thread does not exist");
         }
 
         Thread storage thread = threads[threadID];
 
-        // Allocate a new post that we can append to the thread.
         uint256 postID = allocatePost();
-        require(postID != NULL_REF, "Failed to allocate post.");
+        require(postID != NULL_REF, "failed to allocate post");
 
         Post storage post = posts[postID];
 
         if (createThread) {
-            uint256 pageID = allocateMemory();
-            require(pageID != NULL_REF, "Failed to allocate memory.");
+            uint256 pageID = allocatePage();
+            require(pageID != NULL_REF, "failed to allocate memory");
 
             post.pageID = uint32(pageID);
             post.offset = 0;
@@ -378,23 +342,23 @@ contract Forum {
 
             // Compute the remaining number of words in the most recently allocated page.
             // Finish filling the page before requesting a new page be allocated.
-            uint256 write = maximumContentLength-pageOffset;
+            uint256 write = maximumContentLength - pageOffset;
             if (write > content.length) {
                 write = content.length;
             }
 
-            for(uint256 i = 0; i < write; i++) {
-                pages[thread.pagesTail].words[pageOffset+i] = content[i];
+            for (uint256 i = 0; i < write; i++) {
+                pages[thread.pagesTail].words[pageOffset + i] = content[i];
             }
 
             // If the currently allocated page wasn't large enough to store the post
             // then allocate a new page and write the remaining data.
             if (write < content.length) {
-                uint256 splashID = allocateMemory();
-                require(splashID != NULL_REF, "Failed to allocate memory");
+                uint256 splashID = allocatePage();
+                require(splashID != NULL_REF, "failed to allocate memory");
 
                 for (uint256 i = 0; i < content.length - write; i++) {
-                    pages[splashID].words[i] = content[write+i];
+                    pages[splashID].words[i] = content[write + i];
                 }
 
                 uint256 firstPageID = splashID;
@@ -423,5 +387,105 @@ contract Forum {
                 thread.count = uint32(thread.count + 1);
             }
         }
+
+        return threadID;
+    }
+
+    // AllocateMemory allocates or reserves a single page of memory and returns
+    // its identifier. If a page could not be allocated then this function
+    // returns NULL_REF.
+    //
+    // TODO(271): There has to be a way to bundle all the allocate functions.
+    function allocatePage() isInitialized private returns (uint256 id) {
+        uint256 id = unallocatedPages.head;
+
+        if (id == NULL_REF) {
+            return NULL_REF;
+        }
+
+        if (id == unallocatedPages.tail) {
+            unallocatedPages.head = NULL_REF;
+            unallocatedPages.tail = NULL_REF;
+        } else {
+            unallocatedPages.head = pages[id].next;
+        }
+
+        return id;
+    }
+
+    function allocatePost() isInitialized private returns (uint256 id) {
+        uint256 id = unallocatedPosts.head;
+
+        if (id == NULL_REF) {
+            return NULL_REF;
+        }
+
+        if (id == unallocatedPosts.tail) {
+            unallocatedPosts.head = NULL_REF;
+            unallocatedPosts.tail = NULL_REF;
+        } else {
+            unallocatedPosts.head = posts[id].next;
+        }
+
+        return id;
+    }
+
+    function allocateThread() isInitialized private returns (uint256 id) {
+        uint256 id = unallocatedThreads.head;
+
+        if (id == NULL_REF) {
+            return NULL_REF;
+        }
+
+        if (id == unallocatedThreads.tail) {
+            unallocatedThreads.head = NULL_REF;
+            unallocatedThreads.tail = NULL_REF;
+        } else {
+            unallocatedThreads.head = threads[id].next;
+        }
+
+        return id;
+    }
+
+    function packPost(
+        uint256 value,
+        address author,
+        uint256 next,
+        uint256 pageID,
+        uint256 offset,
+        uint256 length,
+        uint256 flags
+    ) public pure returns (uint256) {
+        // [ 0-20] address: Author
+        // [20-24]  uint32: Next
+        // [24-28]  uint32: Page
+        // [28-30]  uint16: Offset
+        // [30-32]  uint16: Length
+
+        return 0;
+    }
+
+    function packThread(
+        uint256 value,
+        uint256 next,
+        uint256 prev,
+        uint256 pagesHead,
+        uint256 pagesTail,
+        uint256 postsHead,
+        uint256 postsTail,
+        uint256 count,
+        uint256 offset,
+        uint256 flags
+    ) public pure returns (uint256) {
+        // [00-04] uint32: Next
+        // [04-08] uint32: Prev
+        // [08-12] uint32: PagesHead
+        // [12-16] uint32: PagesTail
+        // [16-20] uint32: PostsHead
+        // [20-24] uint32: PostsTail
+        // [24-26] uint16: Count
+        // [26-28] uint16: Offset
+        // [28-32]       : Reserved
+        return 0;
     }
 }
